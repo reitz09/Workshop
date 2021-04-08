bebb8867a3cd635d55958e4aaf16fc3aec4af636
reitz09 <48752893+reitz09@users.noreply.github.com>
2021-04-08 16:21:17 +0100

    Add files via upload
    Adding initial code

---
+++ youtubeSearch.js
@@ -0,0 +1,12 @@
+const{Browser, By, until, Key, Window, Builder}= require ("selenium-webdriver");
+
+function youtubeSearch(){
+let driver = new Builder().forBrowser("chrome").build();
+
+
+
+
+
+
+}
+youtubeSearch()
